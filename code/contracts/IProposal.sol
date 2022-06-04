d6a97761108c2dd50549e3898503e998d0ead6e3
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2022-06-04 21:59:52 +0100

    analysis challenge done, started code challenge, started tests challenge

--- code/contracts/IProposal.sol
+++ code/contracts/IProposal.sol
@@ -1,10 +1,11 @@
-//// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 interface IProposal is IERC1155Receiver {
     event Accept(
@@ -28,7 +29,9 @@ interface IProposal is IERC1155Receiver {
         uint256 amount,
         uint256 price
     );
+
     event List(uint256 saleId, address seller, uint256 assetId, uint256 amount);
+
     event CancelProposal(
         uint256 saleId,
         uint256 proposalId,
@@ -36,6 +39,7 @@ interface IProposal is IERC1155Receiver {
         address buyer,
         uint256 assetId
     );
+
     event Cancel(
         uint256 saleId,
         address seller,
@@ -57,4 +61,49 @@ interface IProposal is IERC1155Receiver {
     function cancelPropose(uint256 _saleId, uint256 _proposalId) external;
 
     function cancelListing(uint256 _saleId) external;
+}
+
+contract TokenProposals {
+    using Counters for Counters.Counter;
+    Counters.Counter private _salesIDs;
+    Counters.Counter private _proposalIDs;
+
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct Proposal {
+        address owner;
+        address buyer;
+        uint256 assetId;
+        uint256 price;
+    }
+
+    function list(uint256 _assetId, uint256 _amount) external {}
+
+    function propose(uint256 _saleId, uint256 _amount)
+        external
+        payable
+        returns (bool)
+    {
+        // initialize variables and function logic
+
+        // emit event
+        emit Propose(
+            saleId,
+            proposalId,
+            msg.sender,
+            "buyer",
+            "assetId",
+            amount,
+            price
+        );
+    }
+
+    function accept(uint256 _saleId, uint256 _proposalId)
+        external
+        returns (bool)
+    {}
 }
--- tests/test/index.ts
+++ tests/test/index.ts
@@ -1,8 +1,38 @@
-import {ethers} from "hardhat";
-import {expect} from "chai";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+import { Contract } from "ethers";
+import "@nomiclabs/hardhat-waffle";
 
 describe("AuctionSale", function () {
-  beforeEach(async function () {});
+  // general setup code - getting accounts, and contract instance
+  let contract: Contract;
 
-  describe("list", function () {});
+  beforeEach(async function () {
+    const AuctionSale = await ethers.getContractFactory("AuctionSale");
+    const auctionSale = await AuctionSale.deploy(
+      "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+    );
+    contract = await auctionSale.deployed();
+  });
+
+  // describe block, insert 'it' blocks here
+  describe("list", function () {
+    // make sure the signer is marked as the auction seller
+    it("marks caller as the auction seller", async () => {
+      const test = (await contract.signer) === contract.seller;
+      expect(test);
+    });
+    // make sure we can create an auction with the list function
+    it("can create an auction with the list function", async () => {
+      // const test = await contract.list();
+      // expect(test).to.equal();
+    });
+
+    // make sure we can bid on an auction with the bid function
+
+    // make sure we can cancel a listing that didn't already start with the cancelList function
+
+    // make sure we can settle a finished auction with the settle function
+  });
 });
